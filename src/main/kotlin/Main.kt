1511bf7cc9c1a94c0c7242f06c64a310c8fc2eda
aaxlss <axl9305@gmail.com>
2021-07-27 22:52:37 +0100

    Explaining when conditions. this is like the switch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,33 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val nombreColor = "Amarillo"
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+    when (nombreColor) {
+        "Amarillo" -> {
+            println("Amarillo es un color amarillo")
+        }
+        "Rojo", "Carmesi" -> println("Rojo es un color rojo") //utilizado para valores con el mismo resultado
+
+
+        else -> println("Error, no info del color")
+    }
+
+    val codigo = 200
+
+    when(codigo){
+        in 200..299 -> println("todo ha ido bien")
+        in 400..500 -> println("ALgo ha fallado")
+        else -> println("codigo desconocido")
+    }
+
+    val tallaDeZapatos = 41
+
+    val mensaje = when(tallaDeZapatos){
+        41,43 -> "tenemos disponible"
+        42, 44 -> "Casi no hay"
+        45 -> "no hay"
+        else -> "solo hay tallas 41, 42, 43 ,44, 45"
+    }
+
+    println(mensaje)
+
 }
