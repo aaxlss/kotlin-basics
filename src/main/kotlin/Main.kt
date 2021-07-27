3d291a49020a358856abc5d8a35a010e7a5487d3
aaxlss <axl9305@gmail.com>
2021-07-27 22:59:16 +0100

    Creatin the loops do and do-while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
 
+    var contador = 10
+
+    while (contador > 0){
+        println("El valor del contador es $contador")
+        contador --
+    }
+
+    do {
+        println("Generando numero aleatorio")
+        val numeroAleatorio = (0..100).random()
+
+        println("El numero generado es $numeroAleatorio")
+    } while (numeroAleatorio > 50)
 }
