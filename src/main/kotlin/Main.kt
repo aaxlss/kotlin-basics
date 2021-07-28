3c887bde8f746f1ce9872dc31002e9f658c44943
aaxlss <axl9305@gmail.com>
2021-07-28 22:45:18 +0100

    Creating mutable and unmutable lists and arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,40 @@
 fun main(args: Array<String>) {
 
+    val listName = listOf("aaa", "xxxxx", "eeeeee") // list no mutable
+
+    println(listName)
+
+    val listaVacia = mutableListOf<String>() //lista mutable
+
+    println(listaVacia)
+    listaVacia.add("rrrrr")
+    println(listaVacia)
+
+    val valor =listaVacia.get(0)
+    println(valor)
+
+    val usandoOperador = listaVacia[0]
+    println(usandoOperador)
+
+
+    val primerValor: String? = listName.firstOrNull() //si la lista esta vacia se retornara un null
+    println(primerValor)
+
+    listaVacia.removeAt(0)
+
+    println(listaVacia)
+
+    listaVacia.add("78979789")
+
+    listaVacia.removeIf { option -> option.length> 3 }
+
+    println(listaVacia)
+
+
+//  Arrays
+
+    val myArray = arrayOf(1,2,3,4)
+    println("myArray: $myArray")
+    println("myArray: ${myArray.toList()}")
+
 }
