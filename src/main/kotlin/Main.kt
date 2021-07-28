98917b5be3158c82bb2f98aeb0647be2818d1565
aaxlss <axl9305@gmail.com>
2021-07-28 23:16:52 +0100

    Creating map objects

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
 fun main(args: Array<String>) {
 
+    var heroes = mapOf(
+        "iron" to 34,
+        "spider" to 32,
+        "america" to 12
+    ) // map object unmutable
+     println(heroes)
+
+    var mapMutable = mutableMapOf(
+        "iron" to 34,
+        "spider" to 32,
+        "america" to 12
+    )
+
+    println(mapMutable)
+
+    mapMutable.put("verde", 789)
+
+    println(mapMutable)
+
+
+    mapMutable["storm"] = 40
+    println(mapMutable)
+
+    var iron = mapMutable["iron"]
+    println(iron)
+
+    mapMutable.remove("storm")
+
+    println(mapMutable.keys)
+    println(mapMutable.values)
 }
