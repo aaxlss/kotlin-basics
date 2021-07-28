55d99f4244cabc31c435de9194df95e354c7cb61
aaxlss <axl9305@gmail.com>
2021-07-28 22:57:27 +0100

    sorting list in different ways asc, desc, shuffling, reversed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
 fun main(args: Array<String>) {
 
+    var lotteryNumbers = listOf(345,345,234,567,768,789,4,8,9,7,1)
+
+    var numeroSort = lotteryNumbers.sorted()
+
+    println(numeroSort)
+
+
+    var numerosDec = lotteryNumbers.sortedDescending()
+
+    println(numerosDec)
+
+    var orderMultiplos = lotteryNumbers.sortedBy { number -> number < 50 } //ordena numeros mayores a 50 primero y deja los menores al final
+
+    println(orderMultiplos)
+
+    var randomSort = lotteryNumbers.shuffled()
+    println(randomSort)
+
+    var reverseList = lotteryNumbers.reversed()
+    println(reverseList)
+
+
+
+
 }
