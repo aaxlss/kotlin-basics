660e1c613df12fc742d9d54b19e8554cf2ebe51d
aaxlss <axl9305@gmail.com>
2021-07-28 22:08:34 +0100

    Using for, for each, map and filter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
 
+    val list = listOf("11a4444","b444444444422", "c33")
+
+    for (option in list){
+        println("option: $option")
+    }
+
+    list.forEach { option -> println("option: $option") }
+
+
+    val characterOptions: List<Int> = list.map { option -> option.length }
+    println("length: $characterOptions")
+
+    var listaFiltrada = list.filter { option -> option.length > 5 }
+    println("filtrados: $listaFiltrada")
 }
