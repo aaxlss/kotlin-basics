eee84935bf37f3ab69a4c6a074be9f2c9536acfe
aaxlss <axl9305@gmail.com>
2021-07-28 22:25:13 +0100

    Using try-catch-finally

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
+    var nombre: String? = "null" //puede ser nulo con el signo ?
+    println(nombre?.length)
+    try {
+        throw NullPointerException("Error con null")
+        nombre!!.length
+    } catch (e : NullPointerException){
+        println("Error: $e")
+    } finally {
+        println("Termino la aplicacion")
+    }
 
+
+    var primerValor = 0
+    var segundoValor = 10
+    var resultado : Int = try {
+        segundoValor / primerValor
+    } catch (e : Exception){
+        0
+    }
+
+    print("rseultado: $resultado")
 }
