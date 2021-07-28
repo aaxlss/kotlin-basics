2fcedd3402541ce203f797d7b41a5ca9d84a47d0
aaxlss <axl9305@gmail.com>
2021-07-28 22:30:47 +0100

    Creating elvis operator when a result is null will assign the value from the right size

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
 
+    var nombre: String? = null
+
+    val characterNombre : Int = nombre?.length ?: 0 //if the result is null, it will take the value from the right
+
+    println("value: $characterNombre")
 }
