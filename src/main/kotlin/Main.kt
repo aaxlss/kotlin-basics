b781a8a5604faff94a77880b226a154ff343e8f7
aaxlss <axl9305@gmail.com>
2021-07-29 23:12:45 +0100

    Creating let function to prected from NullPointerException errors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
+    //la funcion Let permite ejecutar una variable cuando la variable no es nula. Esto para proteger the nullpointerException
 
+    val nombre : String? = "Axel"
+    nombre?.let {
+        valor -> println("El resultado es $nombre")
+    }
 }
