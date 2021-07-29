cee2f314de9bf611851c4d10f0b10a7221c9c674
aaxlss <axl9305@gmail.com>
2021-07-29 23:31:38 +0100

    Creating scope apply function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
+//permite applicar operaciones sobre una variable y luego regresar el valor
 
+    val mobiles = mutableListOf("g 1", "g2", "H 1", "H 2", "X 2")
+        .apply {
+            removeIf { option -> option.contains("g") }
+        }
+    println(mobiles)
+
+    val colores : MutableList<String> = mutableListOf("amarillo", "azul", "rojo")
+
+    colores?.apply {
+        println("El color $this")
+        println("longitud ${this.size}")
+    }
 }
