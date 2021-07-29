247326e6466ce9c3c52f558cefc5b29f3cc715fb
aaxlss <axl9305@gmail.com>
2021-07-29 23:35:29 +0100

    Creating scope also

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
+//permite obtener una vairbale y ejecutar codigo con esa varible que puede ser utilizado con otro funcion
+    var list = mutableListOf<Int>(1,2,3,4,5,6,).also {
+        list -> println("Valor origindal $list")
+    }.asReversed()
 
+    println(list)
 }
