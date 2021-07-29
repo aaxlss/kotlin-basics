a8c39f2b7b0cc020d26dda86cc5f7e14c81dc192
aaxlss <axl9305@gmail.com>
2021-07-29 23:15:59 +0100

    creating scrope function With to accesss to the variable attributes directly

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
+//acceder a las propiedades directamente de una variable
 
+    val colores = listOf("azul", "amarill", "rojo")
+
+    with(colores){
+        println("Los colores son $this")
+        println("Cantidades de colores ${this.size}")
+    }
 }
