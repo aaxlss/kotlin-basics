4fe987bda29190d76b21f8432c8266b4406b77c5
aaxlss <axl9305@gmail.com>
2021-07-29 22:49:11 +0100

    Creating function with parameters and value by default

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
 
+    imprirNombre("Axel", "Sanchez")
+    imprirNombre(nombre="Axel", apellido="Sanchez")
+}
+
+fun imprirNombre (nombre: String,segundoNombre:String ="", apellido: String){
+    println("Mi nombre $nombre $segundoNombre $apellido")
 }
