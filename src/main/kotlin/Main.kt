26b6f338b3b45ad5981f2f84a8c10e06adad5498
aaxlss <axl9305@gmail.com>
2021-07-29 22:58:20 +0100

    Creaing lambda function and applying in another function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,16 @@
 fun main(args: Array<String>) {
 
+
+    var myLambda : (String) -> Int = {
+        valor -> valor.length
+    }
+
+    val lambdaValue = myLambda("String")
+    println(lambdaValue)
+
+    val saludos = listOf("Hola", "Hi")
+
+    val longitudSaludos = saludos.map(myLambda)
+    println(longitudSaludos)
+
 }
