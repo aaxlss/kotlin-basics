0a82c8a8d34f90805e269f728fc98aa27d392107
aaxlss <axl9305@gmail.com>
2021-07-29 21:48:05 +0100

    Creating mutable and unmutable set and getting the first value after a condition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
 fun main(args: Array<String>) {
+//set no tienen elementos repetidos solo se cuenta el primero
+    var vocales = setOf("a","a","c")
 
+    println(vocales)
+
+    var mutables = mutableSetOf(1,2,3,4,5,6,7,8,9)
+    println(mutables)
+    mutables.add(10)
+    mutables.add(1)
+    println(mutables)
+
+    mutables.remove(6)
+    println(mutables)
+
+    val valorDelSet: Int? = mutables.firstOrNull() { number -> number > 2 }
+    println(valorDelSet)
 }
