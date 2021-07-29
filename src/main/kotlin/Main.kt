195f2db059508f93706adbb0c04dcfe46b61dd36
aaxlss <axl9305@gmail.com>
2021-07-29 22:38:48 +0100

    Creatin unit, normal and extention functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,35 @@
 fun main(args: Array<String>) {
 
+    val frase = "Platzi".randomCase()
+
+    printFrase(frase)
 }
+//esta funcion es de tipo Unit, se puede especificar el tipo de retorno unit o no. Esta funcion no tienen la palabra reservada return
+fun printFrase(frase: String):Unit{
+    println(frase)
+}
+
+//Esta es una funcion de tipo Extension. Esta pertenece al objeto que se indica antes del nombre
+fun String.randomCase() : String{
+    val randomNumber = 0..99
+    val random = randomNumber.random()
+
+    return if(random.rem(2) == 0){
+        this.uppercase()
+    } else{
+        this.lowercase()
+    }
+}
+
+
+//esta funcion retorna un resultado de tipo String. (Funcion normal)
+//fun randomCase(frase: String) : String{
+//    val randomNumber = 0..99
+//    val random = randomNumber.random()
+//
+//    return if(random.rem(2) == 0){
+//        frase.uppercase()
+//    } else{
+//        frase.lowercase()
+//    }
+//}
