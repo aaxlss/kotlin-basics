7f628ba7b907f88a35ec58caaac981ac854c5a00
aaxlss <axl9305@gmail.com>
2021-07-29 23:07:15 +0100

    Creating function with parameter as a function, creating a function that returns another function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,21 @@
 fun main(args: Array<String>) {
 
+var largoValorInit = superFuncion("platzi") { value ->
+    value.length
+}
+    println(largoValorInit)
+
+    val lambda = inception("Axel")
+    val valorLambda = lambda()
+    println(valorLambda)
+}
+
+fun superFuncion(valorInit : String, block: (String) -> Int):Int{
+    return block(valorInit)
+}
+
+fun inception(nombre: String) : () ->String{
+    return {
+        "Hola desde lambda $nombre"
+    }
 }
