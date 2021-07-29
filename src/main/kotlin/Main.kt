d3e337c31b92d6eb30fb0695abba9e6a4d361537
aaxlss <axl9305@gmail.com>
2021-07-29 23:21:11 +0100

    Creating scope run function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
+//permite ejecutar una serie de operaciones despues de declarar una variable
+    var phones = mutableListOf("Google Pixel 2XL", "Google Pixel 4a", "Huawei Redmi 9", "Xiaomi mi a3" )
+        .run{
+            removeIf { phone -> phone.contains("Google") }
+            this
+        }
 
+    println(phones)
 }
